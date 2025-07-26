5d4599549127867f8e06d380c5b131efc22c6c9c
EmersonBerido <beridoemerson05@gmail.com>
2025-07-26 11:53:53 -0700

    Update AddTransaction.jsx
    newTransaction object fully made; still needs encryption and being added to the local storage

--- src/AddTransaction.jsx
+++ src/AddTransaction.jsx
@@ -2,11 +2,33 @@ function AddTransaction() {
   //add transactions in userInfo array of objects in local storage
   //objects will contain {store/Location name (will be the title of the object), what was bought, if it was an income or expense, amount used, date, category}}
 
+  function handleSubmit(event){
+    //stops refreshing
+    event.preventDefault();
+
+    //creats object of new transaction values
+    const newTransaction = {
+      store : event.target.store.value,
+      item : event.target.item.value,
+      transactionType : event.target.transactionType.value,
+      amount : event.target.amount.value,
+      date : {
+        day : event.target.day.value,
+        month : event.target.month.value,
+        year : event.target.year.value,
+      },
+      category : event.target.category.value
+    }
+    console.log(newTransaction);
+
+    //stores new transaction in local storage
+    //stored in stack??
+  }
 
   return (
     <section style = {{border : "1px solid green"}}>
       <h1>Add Transaction</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label for="store">Store</label>
         <input type="text" name="store" id="store" placeholder="Target" required/>
 
@@ -20,7 +42,7 @@ function AddTransaction() {
         </select>
 
         <label for="amount">Amount</label>
-        <input type="number" inputMode="numeric" name = "amount" id="amount" min="0" required/>
+        <input type="number" inputMode="numeric" name = "amount" id="amount" min="0" step="0.01" required/>
 
         <section className="date">
           <label for="day">Day</label>
